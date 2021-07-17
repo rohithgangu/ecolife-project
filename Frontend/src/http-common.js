7a60dc466949dbb4df87460d3ccb9459ab38e394
rohithgangu <55388632+rohithgangu@users.noreply.github.com>
2021-07-17 14:59:32 +0530

    end of it 2

--- Frontend/src/http-common.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/products",
-    headers:{
-        "content-type": "application.json"
-    }
-});
